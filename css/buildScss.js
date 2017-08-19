c73e85e544f78cb6abf4244ae7f37db2182ccbf9
Ben Coveney <bencoveney@gmail.com>
2017-08-19 22:57:06 +0100

    fixed build

--- css/buildScss.js
+++ css/buildScss.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 const sass = require("node-sass");
 const css = sass.renderSync({ file: "css/styles.scss" }).css;
